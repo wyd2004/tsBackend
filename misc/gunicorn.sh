7cb234e454273e1504f33b823eeb1ab0f1608842
YidingW <wangyiding.chinese@gmail.com>
2016-11-23 00:40:09 +0800

    add server config

---
+++ misc/gunicorn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source env/bin/activate
+
+cd src/tscast
+
+exec gunicorn -k gevent tscast.wsgi:application \
+    --name mjtt_django\
+    --bind 127.0.0.1:4001\
+    --workers 3 \
+    --log-level=info \
+    #--log-file=/var/log/gunicorn/gunicorn.log \
+    #--access-logfile=/var/log/gunicorn/access.log \
+    "$@"
