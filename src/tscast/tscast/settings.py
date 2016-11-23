52570d22b2715c1577e03c52e849032bbf677b65
YidingW <wangyiding.chinese@gmail.com>
2016-11-23 21:32:29 +0800

    Add zh_CN language.

--- src/tscast/tscast/settings.py
+++ src/tscast/tscast/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v5#gix1jc-r!n#mqy!ky6ey7!sf@b+(44fy=d)q8h8n)axu)$i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True and False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['120.25.232.11',]
 
 
 # Application definition
