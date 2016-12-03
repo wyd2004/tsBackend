39873d87a50b9fb76ea843dce8d770922b75bf31
YidingW <wangyiding.chinese@gmail.com>
2016-12-03 16:00:42 +0800

    fix missing import `Member`

--- src/tscast/member/utils/permissions.py
+++ src/tscast/member/utils/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions
+from member.models import Member
+
+
 class OnlyMemberAccess(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return type(request.user) is Member
