055e1dfcaae24b78c21094592e564f530ff2275a
YidingW <wangyiding.chinese@gmail.com>
2016-12-11 14:10:08 +0800

    alter deploy

--- misc/docker/gunicorn.sh
+++ misc/docker/gunicorn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #source env/bin/activate
-#mkdir -p /var/log/gunicorn/
-#mkdir -p /var/log/supervisor/
+mkdir -p /var/log/gunicorn/
+mkdir -p /var/log/supervisor/
 
 exec gunicorn -c misc/gunicorn.conf tscast.wsgi:application
--- src/tscast/tscast/settings.py
+++ src/tscast/tscast/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            # 'charset': 'utf8mb4',
+            'charset': 'utf8mb4',
         },
     },
 }
