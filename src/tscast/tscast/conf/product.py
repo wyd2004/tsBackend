053813447d05071a25a4b95ede59eafc387e2446
YidingW <wangyiding.chinese@gmail.com>
2016-12-18 12:24:11 +0800

    gun dep

--- src/tscast/tscast/conf/product.py
+++ src/tscast/tscast/conf/product.py
@@ -1,6 +1,7 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'charset': 'utf8mb4'},
         'NAME': 'tscast',
         'USER': 'dbuser',
         'PASSWORD': '123456abc',
