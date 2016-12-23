a397ec67ecfe5ee59515e71632026ad3f7d7dfc2
YidingW <wangyiding.chinese@gmail.com>
2016-12-23 23:38:26 +0800

    media url domain

--- src/tscast/tscast/settings.py
+++ src/tscast/tscast/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'tscast/static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'http://vip.tangsuanradio.com/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'tscast/media/')
 
