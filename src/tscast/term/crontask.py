8cb0e18a421e39a75f6a2a6cf9f30fa3c07b084a
YidingW <wangyiding.chinese@gmail.com>
2017-01-19 00:04:39 +0800

    wx pay stat query

---
+++ src/tscast/term/crontask.py
@@ -0,0 +1,17 @@
+
+from .models import Order
+from .models import Payment
+from .models import Purchase
+from wechat.api import wx_order_query
+
+def payment_status_update():
+    unchecked_payments = Payment.objects.filter(status='wait-for-payment')
+    for wait_pay in unchecked_payments:
+        state = wx_order_query()
+        if state:
+            wait_pay.status = 'done'
+        else:
+            wait_pay.status = 'fail'
+        wait_pay.save(force_update=True)
+
+
