e19e7bfe7440be42ac71f8b5c51076d74dbf8451
YidingW <wangyiding.chinese@gmail.com>
2017-01-19 00:27:22 +0800

    wx pay stat query

--- src/tscast/term/crontask.py
+++ src/tscast/term/crontask.py
@@ -1,4 +1,4 @@
-
+import json
 from .models import Order
 from .models import Payment
 from .models import Purchase
@@ -7,10 +7,17 @@ from wechat.api import wx_order_query
 def payment_status_update():
     unchecked_payments = Payment.objects.filter(status='wait-for-payment')
     for wait_pay in unchecked_payments:
-        state = wx_order_query()
+        pay_receipt = json.loads(wait_pay.receipt)[0]
+        nonce_str = pay_receipt['prepay']['nonce_str']
+        out_trade_no = pay_receipt['prepay']['out_trade_no']
+        sign = pay_receipt['prepay']['sign']
+        state = wx_order_query(nonce_str, out_trade_no, sign)
         if state:
             wait_pay.status = 'succeeded'
         else:
             wait_pay.status = 'failed'
         wait_pay.save(force_update=True)
+        Order.objects.filter(uuid=wait_pay.uuid).update(
+            status=wait_pay.status
+        )
 
