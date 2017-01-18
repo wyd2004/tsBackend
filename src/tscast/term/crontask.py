11aec3e25160333b71f7004b81e5d0e5553350e7
YidingW <wangyiding.chinese@gmail.com>
2017-01-19 00:06:24 +0800

    wx pay stat query

--- src/tscast/term/crontask.py
+++ src/tscast/term/crontask.py
@@ -9,9 +9,8 @@ def payment_status_update():
     for wait_pay in unchecked_payments:
         state = wx_order_query()
         if state:
-            wait_pay.status = 'done'
+            wait_pay.status = 'succeeded'
         else:
-            wait_pay.status = 'fail'
+            wait_pay.status = 'failed'
         wait_pay.save(force_update=True)
-
 
