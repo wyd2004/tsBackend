93373c8ca57d73c0acbca114dea675e22ac89f81
YidingW <wangyiding.chinese@gmail.com>
2017-01-26 10:55:45 +0800

    REST CONTENT PARSER

--- src/tscast/tscast/settings.py
+++ src/tscast/tscast/settings.py
@@ -292,6 +292,11 @@ REST_FRAMEWORK = {
         'DEFAULT_PERMISSION_CLASSES': (
             'tscast.utils.permissions.ReadOnly',
             ),
+        'DEFAULT_PARSER_CLASSES': (
+            'rest_framework.parsers.JSONParser',
+            'rest_framework.parsers.FormParser',
+            'rest_framework.parsers.XMLParser',
+        ),
         }
 
 
