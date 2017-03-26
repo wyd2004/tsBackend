b350e64dfb48593e9f894d426d3828fe84438cd2
YidingW <wangyiding.chinese@gmail.com>
2017-03-26 23:00:23 +0800

    pay exp

--- src/tscast/member/models.py
+++ src/tscast/member/models.py
@@ -218,7 +218,6 @@ class Privilege(object):
                 payload['album'] = ''
                 payload['episode'] = purchase_object.id
                 self.episode_ids.add(payload['episode'])
-                self.channel_ids = []
             yield purchase.package, payload
 
     def dumps(self):
